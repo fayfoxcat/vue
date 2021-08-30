7214701e1776ce0fb54120a74f1fcaa11a571b28
fox <fox@fox.com>
2021-08-30 22:00:23 +0800

    添加vuex 和router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <HelloWorld msg="欢迎" />
-  <Fox/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import Fox from "./components/Fox.vue";
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    Fox,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -22,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
