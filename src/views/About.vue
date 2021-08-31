ed0cd36f481d1225ab105d1b050ccfd2cc45dbad
fox <fox@gamil.com>
2021-08-31 18:39:27 +0800

    11

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
+  <div id="about">
     <h1>This is an about page</h1>
+    <button @click='show'>显示</button>
   </div>
 </template>
+<script>
+import {createApp} from "vue";
+
+export default {
+
+}
+
+createApp({
+  data() {
+    return {
+      id: 2021,
+      type: 'top'
+    }
+  },
+  methods: {
+    show() {
+      console.log(this.type)
+    }
+  }
+}).mount("#about")
+</script>
+
+<style scoped lang="scss">
+
+</style>
