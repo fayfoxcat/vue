536488b01b58345330a4ffeff313d26566183c11
fox <fox@gamil.com>
2021-09-01 17:58:40 +0800

    11

--- src/components/navHeader/NavHeader.vue
+++ src/components/navHeader/NavHeader.vue
@@ -1,14 +1,57 @@
 <template>
+  <div id="header">
+    <div>{{ num }}</div>
+    <div>{{ name }}</div>
+    <div>{{ obj }}</div>
+    <button @click.once="print(10)">打印obj</button>
     <div>
-        header
+      <a :[vName]="doSomeing">hello</a>
+      <button @[eventName]="updateHref">修改节点功能</button>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
+import {createApp, reactive, ref, toRefs} from "vue";
 
+export default {
+  setup() {
+    let num = ref(10)
+    let name = ref('jack')
+    let data = reactive({
+      arr: [1, 2, 3],
+      obj: {
+        id: 2021,
+        name: '徐凤年'
+      },
+      vName: '',
+      eventName: 'click',
+      doSomeing: 'https://www.baidu.com'
+    })
+    let print = (val) => {
+      console.log(data.obj.id + val)
+    }
+    let updateHref = () => {
+      console.log('updateHref');
+      data.vName = 'href'
+      data.eventName = 'focus'
+    }
+    return {
+      num,
+      name,
+      ...toRefs(data),
+      print,
+      updateHref
+    }
+  },
 }
-    
+const app = createApp({
+  data() {
+    return {count: 4}
+  }
+})
+const vm = app.mount("#header")
+console.log(vm);
 </script>
 
 <style scoped lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <div>{{num}}</div>
-    <div>{{name}}</div>
-    <!-- <div>{{data.obj}}</div> -->
-    <div>{{obj}}</div>
-    <button @click="print(10)">打印obj</button>
-
     <nav-header></nav-header>
     <nav-main></nav-main>
     <nav-footer></nav-footer>
@@ -14,7 +7,7 @@
 </template>
 
 <script>
-import { defineComponent ,ref ,reactive,toRefs} from "vue";
+import { defineComponent} from "vue";
 import NavHeader from "@/components/navHeader/NavHeader";
 import NavMain from "@/components/navMain/NavMain";
 import NavFooter from "@/components/navFooter/NavFooter";
@@ -27,26 +20,5 @@ export default defineComponent({
     NavFooter,
   },
 
-  setup() {
-    let num = ref(10)
-    let name = ref('jack')
-    let data = reactive({
-      arr:[1,2,3],
-      obj:{
-        id:2021,
-        name:'徐凤年'
-      }
-    })
-    let print = (val)=>{
-        console.log(data.obj.id+val)
-    } 
-    return {
-      num,
-      name,
-      // data,
-      ...toRefs(data),
-      print
-    }
-  },
 });
 </script>
