7f87073edbc8923b19a5da417b5150b1df2d32e7
fox <fox@fox.com>
2021-09-01 08:07:48 +0800

    方法和数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <div>{{num}}</div>
+    <div>{{name}}</div>
+    <!-- <div>{{data.obj}}</div> -->
+    <div>{{obj}}</div>
+    <button @click="print(10)">打印obj</button>
+
     <nav-header></nav-header>
     <nav-main></nav-main>
     <nav-footer></nav-footer>
@@ -8,7 +14,7 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent ,ref ,reactive,toRefs} from "vue";
 import NavHeader from "@/components/navHeader/NavHeader";
 import NavMain from "@/components/navMain/NavMain";
 import NavFooter from "@/components/navFooter/NavFooter";
@@ -18,7 +24,29 @@ export default defineComponent({
   components: {
     NavHeader,
     NavMain,
-    NavFooter
+    NavFooter,
+  },
+
+  setup() {
+    let num = ref(10)
+    let name = ref('jack')
+    let data = reactive({
+      arr:[1,2,3],
+      obj:{
+        id:2021,
+        name:'徐凤年'
+      }
+    })
+    let print = (val)=>{
+        console.log(data.obj.id+val)
+    } 
+    return {
+      num,
+      name,
+      // data,
+      ...toRefs(data),
+      print
+    }
   },
 });
 </script>
