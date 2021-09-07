3bcae7f10a372e82c474b12d35c68b943c69187d
fox <fox@gamil.com>
2021-09-07 15:17:43 +0800

    美化ui

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,6 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 #nav {
   padding: 30px;
@@ -24,7 +17,4 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/components/navMain/NavMain.vue
+++ src/components/navMain/NavMain.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="main">
-    <input placeholder="添加代办事项" v-model="value" @keydown="add_task" />
+    <el-input placeholder="添加代办事项" size="medium" v-model="value" @keydown="add_task">
+    </el-input>
     <div v-for="(item, index) in tasks" :key="index">
       <div class="item">
-        <input type="checkbox" v-model="item.complete" />
-        <span>{{ item.title }}</span>
-        <el-button class="del_task">移除</el-button>
+        <el-checkbox v-model="item.complete" :label="item.title"></el-checkbox>
+        <el-button size="mini" class="del_task">移除</el-button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "@vue/reactivity";
+import {reactive, toRefs} from "@vue/reactivity";
+
 export default {
   setup() {
     let data = reactive({
@@ -46,13 +47,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#main{
+#main {
   margin: 50px auto 0 auto;
   padding: 10px;
   width: 20%;
   background-color: #eeeeee;
+
   .item {
     height: 35px;
+    line-height: 35px;
+
+    .del_task {
+      margin: auto 15px;
+    }
   }
 }
 
