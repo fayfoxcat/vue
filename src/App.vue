50c891a3fcbc0ad643a89e4fcfe06842d3e6c352
fox <fox@gamil.com>
2022-03-31 15:15:56 +0800

    复习

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="nav">
-    <router-link to="/home">Home</router-link> |
-    <router-link to="/">About</router-link>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-
 #nav {
   padding: 30px;
+  border: 1px solid #2c3e;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,19 @@ import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/',
+    path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/fox',
     name: 'Fox',
-    component: () => import('../components/Fox.vue')
+    component: () => import('../components/other/Fox.vue')
   }
 ]
 
